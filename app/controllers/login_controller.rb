606f08c6b52def6e083715956cfacda1d0a9e1fe
Sourav Lahoti <sourav26721062@gmail.com>
2015-09-15 14:15:07 +0530

    Welcome screen Css

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,7 +1,8 @@
 class LoginController < ApplicationController
-
+	before_action :create_client
   def index
   	@user = User.find(session[:user_id])
+  	@tweets = @client.user_timeline(@client.user.id).take(100)
   	if session[:user_id]
   		render :template => 'sessions/index'
   	else
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
 	before_action :create_client
 	def index
+		@user = User.find(session[:user_id])
 		@tweets = @client.user_timeline(@client.user.id).take(100)
 		max_tweet = TweetOwner.count
 		@max = -1
