201e8c2d57050d01e331357f66ff14fee7d3b85e
Sourav Lahoti <sourav26721062@gmail.com>
2015-10-04 13:48:30 +0530

    Update tweets_controller.rb
    Removed Api keys

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -33,10 +33,10 @@ class TweetsController < ApplicationController
 	private
 	def create_client
 		@client = Twitter::REST::Client.new do |config|
-	  		config.consumer_key = 'YXbavRvj6x8NXix57X65Y8xzL'
-	  		config.consumer_secret = 'coW17AfL3CUFcizWfc4GcNGtFLamJWJYTPYK0dNGLkOcugIqAi'
-	  		config.access_token = '971929843-BCHaFeCaIBQbg5baiZpTRFBJiPibTrtOlImV8Kbw'
-	  		config.access_token_secret = 'M2oV1i6h7PXaZVQXmuVlZZC4bv6LpnNe9a9xkH25ulCOf'
+	  		config.consumer_key = ''
+	  		config.consumer_secret = ''
+	  		config.access_token = ''
+	  		config.access_token_secret = ''
 		end
 	end
 
