28e7c456329a8f320ba9c8de2b8d6bb77e450812
Sourav Lahoti <sourav26721062@gmail.com>
2015-10-07 10:58:57 +0530

    Update tweets_controller.rb
    Removed twitter keys

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -33,10 +33,10 @@ class TweetsController < ApplicationController
 	private
 	def create_client
 		@client = Twitter::REST::Client.new do |config|
-	  		config.consumer_key = ''
-	  		config.consumer_secret = ''
-	  		config.access_token = ''
-	  		config.access_token_secret = ''
+	  		config.consumer_key = 'Your consumer key'
+	  		config.consumer_secret = 'Your consumer secret key'
+	  		config.access_token = 'Your access token key'
+	  		config.access_token_secret = 'Your access secret key'
 		end
 	end
 
