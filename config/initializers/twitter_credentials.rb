7378b9096071b832866dd0608b47ae1f0b4d10c6
Sourav Lahoti <sourav26721062@gmail.com>
2015-10-07 10:57:14 +0530

    Update twitter_credentials.rb
    Removed twitter keys

--- config/initializers/twitter_credentials.rb
+++ config/initializers/twitter_credentials.rb
@@ -1,8 +1,8 @@
 def create_client
 	@client = Twitter::REST::Client.new do |config|
-  		config.consumer_key = ''
-  		config.consumer_secret = ''
-  		config.access_token = ''
-  		config.access_token_secret = ''
+  		config.consumer_key = 'Your consumer key'
+  		config.consumer_secret = 'Your consumer secret key'
+  		config.access_token = 'Your access token key'
+  		config.access_token_secret = 'Your access token secret key'
 	end
 end
